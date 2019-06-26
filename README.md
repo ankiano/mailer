79fbcc9b92471bbf08fc5ad26ddac837b959755d
ankiano <a.akinshyn@gmail.com>
2019-06-26 12:16:55 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mailer
 Mailer is a command line tool for sending emails from gmail account.
+
+# Installation:
+    sudo pip3 install git+https://github.com/ankiano/mailer.git -U
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+# coding=utf-8
+
+from pip._internal.req import parse_requirements
+from pip._internal.download import PipSession
+
+from setuptools import setup, find_packages
+
+parsed_requirements = parse_requirements(
+    'requirements.txt',
+    session=PipSession()
+)
+
+requirements = [str(ir.req) for ir in parsed_requirements]
+
+setup(
+    name='mailer',
+    version='0.0.1',
+    description='CLI tool for sending emails',
+    long_description='',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='mailer',
+    entry_points={'console_scripts': ['mailer=mailer:cli']},
+)
